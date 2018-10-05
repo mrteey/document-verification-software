b0d77575f1714ffbf6f7e571cd4736d2e97c69ea
Nasir Mustapha <teboy.com@gmail.com>
2018-10-05 22:04:00 +0100

    added test code to app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 #Main Application
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Its working'
+
+if __name__ == '__main__':
+    app.run(debug=True)

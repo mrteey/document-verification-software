3ce232fd839dc4b35306b15c8fcc967ba128f999
Suleiman Abdulrazaq <suleimanbichi@gmail.com>
2018-10-16 14:58:02 +0100

    Updates

--- templates/scripts/app.js
+++ templates/scripts/app.js
@@ -1,3 +1,19 @@
+// LOGO SWITCH
+$(window).scroll(function() {
+  var hT = $('.feedBack').offset().top,
+      hH = $('.feedBack').outerHeight(),
+      wH = $(window).height(),
+      wS = $(this).scrollTop();
+  if (wS > (hT+hH-wH)){
+    $('.logoArea>img')[0].src = 'http://127.0.0.1:5500/templates/imgs/verifrLogoInverted.png';
+  }
+  if (wS < (hT+hH-wH)){
+    $('.logoArea>img')[0].src = 'http://127.0.0.1:5500/templates/imgs/verifrLogo.png';
+  }
+});
+
+
+// HERO BACKGROUND IMAGE SCROLL
 $('.wrapper').fullClip({
   images: ['imgs/2.jpg', 'imgs/3.jpg', 'imgs/4.jpg', 'imgs/1.jpg'],
   transitionTime: 2000,

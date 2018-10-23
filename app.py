fdb340f55c758da9f9d7ff274fafec01a0eb815f
Nasir Mustapha <teboy.com@gmail.com>
2018-10-23 14:56:05 +0100

    Broke index.html into header, footer, main. Added a test about-us page.

--- app.py
+++ app.py
@@ -9,8 +9,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#This is a test route to show how the footer and header can be included in a new page
+#Ajax can also be used to call this route, in that case, this route will return only the page fragment
+#required to be replaced with on the main template. Thus, instead of including the header and footer
+#in about-us the template we could just return the main about-us content to be replaced with.
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us.html')
+
 #Finally we run the app, __name__ == __main__ means if we are accessing this file directly
-#It is currently on debug mode, to help up catch any error that may come up while the app runs
-#The debug mode will be removed when we are on production so that our users don't to see our errors
+#It is currently on debug mode, to help us catch any error that may come up while the app runs
+#The debug mode will be removed when we are on production so that our users don't get to see our errors
 if __name__ == '__main__':
     app.run(debug=True)
